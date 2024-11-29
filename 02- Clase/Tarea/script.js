25b88ca909776537f85b1667483ad12cce81cebb
pcedones <pcedones@ternium.com.ar>
2024-11-29 15:00:21 -0300

    tarea clase 2

--- 02- Clase/Tarea/script.js
+++ 02- Clase/Tarea/script.js
@@ -105,7 +105,7 @@ console.log(mensaje) */
 /*
 ## Ejercicios Adicionales
 
-13. Pide al usuario que ingrese un número utilizando `prompt`, 
+//VER CON EL PROFE//////////////13. Pide al usuario que ingrese un número utilizando `prompt`, 
 luego verifica si es un número primo e imprime un mensaje correspondiente.
 */
 
@@ -113,12 +113,96 @@ luego verifica si es un número primo e imprime un mensaje correspondiente.
 /*
 14. Crea una variable llamada `hora` y asígnale un valor numérico que represente una hora del día. 
 Luego, utilizando un `if else`, imprime un mensaje que indique si es mañana, tarde o noche.
-15. Pide al usuario que ingrese dos números utilizando `prompt`, luego verifica si el primero es divisible por el segundo e imprime un mensaje correspondiente.
-16. Crea una variable llamada `nombreUsuario` y asígnale un nombre. Luego, verifica si el nombre tiene más de 6 caracteres e imprime un mensaje correspondiente.
-17. Utilizando un `if else`, verifica si un año ingresado por el usuario es bisiesto o no, e imprime un mensaje correspondiente.
+*/
+
+/*
+mañana: 06:00hs a 12.00hs
+tarde: 13:00hs a 19:00hs
+noche: 20:00hs a 05:00hs
+*/
+/* let hora = 23;
+
+if (hora < 13 && hora >5 ){
+    console.log("Es la mañana")
+} else if (hora >= 13 && hora<20){
+    console.log("Es la tarde")
+}else if (hora >=20 || hora <= 5){
+    console.log("es la noche")
+} else {
+    console.log("Ingrese un valor numerico");
+}
+ */
+
+
+/*
+15. Pide al usuario que ingrese dos números utilizando `prompt`, 
+luego verifica si el primero es divisible por el segundo e imprime un mensaje correspondiente.
+*/
+/* let numeroA=parseInt(prompt("ingrese un numero"));
+let numeroB=parseInt(prompt("ingrese un numero"));
+
+let mensaje =( numeroA % numeroB === 0)
+? "Los numeros son divisibles entre si"
+: "Los numeros no son divisibles entre si"
+
+console.log(mensaje) */
+
+
+/*
+16. Crea una variable llamada `nombreUsuario` y asígnale un nombre. 
+Luego, verifica si el nombre tiene más de 6 caracteres e imprime un mensaje correspondiente.
+*/
+/* let nombreUsuario = prompt("Crea un usuario");
+if (nombreUsuario.length >= 6){
+    console.log ("Tu nombre de usuario tiene mas de 6 caracteres")
+}else {
+    console.log("Tu nombre de usuario tiene menos de 6 caracteres")
+} */
+
+
+
+/*
+17. Utilizando un `if else`, verifica si un año ingresado por el usuario es bisiesto o no, 
+e imprime un mensaje correspondiente.
+*/
+/* let ano = parseInt(prompt("ingrese un año"));
+
+if (ano % 4 ===0){
+    console.log("es un año bisiesto por dividirse por 4")
+} else if ( ano % 100 === 0 && ano % 400 === 0){
+    console.log("es un año bisiesto, al dividirlo por 100 y 400")
+}else{
+    console.log("No es un año bisiesto") 
+}
+*/
+/*
 18. Pide al usuario que ingrese su nombre utilizando `prompt`, luego verifica si comienza con la letra "A" o "a" e imprime un mensaje correspondiente.
-19. Declara una variable llamada `temperatura` y asígnale un valor numérico que represente la temperatura actual. Luego, utilizando un `if`, imprime un mensaje que indique si hace frío, templado o caliente.
-20. Pide al usuario que ingrese tres números utilizando `prompt`, luego verifica si la suma de los dos primeros números es igual al tercer número e imprime un mensaje correspondiente.
+*/
+/* let usuario = prompt("decime tu nombre")
 
+if (usuario.charAt(0)=== "a" ){
+    console.log("tu nombre empieza con a minuscula")
+}else if ( usuario.charAt(0)==="A") {
+    console.log("tu nombre empieza con a mayuscula")
+}else{
+    console.log("tu nombre no comienza con A o a")
+} */
 
+/*19. Declara una variable llamada `temperatura` y asígnale un valor numérico que represente la temperatura actual. 
+Luego, utilizando un `if`, imprime un mensaje que indique si hace frío, templado o caliente.
+*/
+/* let temperatura = 9;
+if (temperatura >= 25){
+    console.log("Hace calor")
+} else if (temperatura <25 && temperatura >=11){
+    console.log("esta templado")
+} else if ( temperatura <10){
+    console.log ("hace frio")
+}else{
+    console.log("clima indefinido")
+}
+ */
+/*
+20. Pide al usuario que ingrese tres números utilizando `prompt`, 
+luego verifica si la suma de los dos primeros números es igual al tercer número e imprime un mensaje correspondiente.
  */
